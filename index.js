f2b514e946b3f1936cdb7df172d198e638802dc2
Pooya Parsa <pooya@pi0.ir>
2017-07-20 14:01:50 +0430

    chore: lint

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 // Default options
 const defaults = {
-  xForwardedProto: true,
-  redirectPort: 443,
-  redirectHost: undefined,
-  statusCode: 307
+    xForwardedProto: true,
+    redirectPort: 443,
+    redirectHost: undefined,
+    statusCode: 307
 }
 
 function isEmpty(v) {
@@ -14,25 +14,23 @@ function isEmpty(v) {
 // Creates new middleware using provided options
 function create(options) {
     const { xForwardedProto, redirectPort, redirectHost, statusCode } = Object.assign({}, defaults, options)
-
     const _port = redirectPort === 443 ? '' : (': ' + redirectPort)
 
     return function redirectSSL(req, res, next) {
-
         // Test using req.connection.encrypted
-        const _encrypted = isEmpty(req.connection.encrypted) ? null : req.connection.encrypted === true 
+        const _encrypted = isEmpty(req.connection.encrypted) ? null : req.connection.encrypted === true
 
         // Test using req.protocol
         const _httpsProtocol = isEmpty(req.protocol) ? null : req.protocol === 'https'
 
         // Test using x-forwarded-proto header
         const _httpsXforwarded = (!xForwardedProto || isEmpty(req.headers['x-forwarded-proto'])) ? null
-         : req.headers['x-forwarded-proto'].indexOf('https') !== -1
+            : req.headers['x-forwarded-proto'].indexOf('https') !== -1
 
         const _noDetectionMethod = _encrypted === null && _httpsProtocol === null && _httpsXforwarded === null
 
         if (_encrypted || _httpsProtocol || _httpsXforwarded || _noDetectionMethod) {
-                return next()
+            return next()
         }
 
         const ـredirectURL = 'https://' + (redirectHost || req.headers.host) + _port + req.url
