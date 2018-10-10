83867e24d02f660e17fc2e2efb02cd34b6f4aa79
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2018-10-10 05:27:45 -0400

    feat: add exclude options which adds the ability to disable SSL redirect on some routes (#5)

--- index.js
+++ index.js
@@ -2,32 +2,45 @@ const isHTTPS = require('is-https')
 
 // Default options
 const defaults = {
-    xForwardedProto: true,
-    redirectPort: 443,
-    redirectHost: undefined,
-    redirectUnknown: true,
-    statusCode: 307,
-    redirect: process.env.NODE_ENV === 'production'
+  xForwardedProto: true,
+  redirectPort: 443,
+  redirectHost: undefined,
+  redirectUnknown: true,
+  statusCode: 307,
+  redirect: process.env.NODE_ENV === 'production',
+  exclude: []
+}
+
+const isExcluded = function (url, patterns = []) {
+  return patterns.some(pattern => url.match(pattern))
 }
 
 // Creates new middleware using provided options
-function create(options) {
-    const { xForwardedProto, redirectPort, redirectHost, statusCode, redirectUnknown, redirect } = Object.assign({}, defaults, options)
-    const _port = redirectPort === 443 ? '' : (': ' + redirectPort)
+function create (options) {
+  const {
+    xForwardedProto,
+    redirectPort,
+    redirectHost,
+    statusCode,
+    redirectUnknown,
+    redirect,
+    exclude
+  } = Object.assign({}, defaults, options)
+  const _port = redirectPort === 443 ? '' : (': ' + redirectPort)
 
-    return function redirectSSL(req, res, next) {
-        if (redirect) {
-            const _isHttps = isHTTPS(req, xForwardedProto)
-            const shouldRedirect = _isHttps === false || (redirectUnknown && _isHttps === null)
-            if (shouldRedirect) {
-                const ـredirectURL = 'https://' + (redirectHost || req.headers.host) + _port + req.url
-                res.writeHead(statusCode, { Location: ـredirectURL })
-                return res.end()
-            }   
-        }
-
-        return next()
+  return function redirectSSL (req, res, next) {
+    if (redirect && !isExcluded(req.url, exclude)) {
+      const _isHttps = isHTTPS(req, xForwardedProto)
+      const shouldRedirect = _isHttps === false || (redirectUnknown && _isHttps === null)
+      if (shouldRedirect) {
+        const ـredirectURL = 'https://' + (redirectHost || req.headers.host) + _port + req.url
+        res.writeHead(statusCode, { Location: ـredirectURL })
+        return res.end()
+      }
     }
+
+    return next()
+  }
 }
 
 // Create a new instance using defaults
